3e0042f13041c8d97fa13735c81456967d82e05e
Thinh <thinhle.ict@gmail.com>
2025-08-23 09:49:30 +0700

    remove secrets

--- iot/build.sh
+++ iot/build.sh
@@ -1,2 +1,2 @@
-docker login vcr-han.vngcloud.vn -u 106790-reader -p S4KVagSfkjp3RtaadZjlAGeFTt8xzMhT
+docker login vcr-han.vngcloud.vn -u 106790-reader
 docker pull vcr-han.vngcloud.vn/106790-scamera-ai/iot_speaker:latest
